922e438035dabf80320b6125268a73fa9d4ffbf0
kila1 <kila14081996@gmail.com>
2024-02-21 22:37:24 +0300

    Домашняя работа 4. 2 задание.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,11 +7,19 @@ public class Main {
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + " то, он совершеннолетний");
         }
-
         int ageTwo = 17;
         if (ageTwo < 18) {
             System.out.println("Если возраст человека равен " + ageTwo + " то, он не достиг совершеннолетия " +
                     "нужно немного подождать");
         }
+
+        int temperature = 8;
+        if (temperature <= 5) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+        }
+
+
     }
 }
